13e654f38c61031723ef892bc22f8414a6d6ee45
grizzli03 <kurbanovashin@gmail.com>
2023-05-09 07:58:32 +0600

    new rep

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/hello/Desktop/web_project/venv"
+set -gx VIRTUAL_ENV "/home/user/Desktop/aio/Web-Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
